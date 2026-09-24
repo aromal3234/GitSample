9574d6bc288761034179bf159583a78d8fe1712c
aromal3234 <aromal3234@gmail.com>
2026-09-24 14:42:34 +0530

    Updated Sample class

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,11 @@
 
 public class Sample {
+	void show() {
+		System.out.println("Hello Aromal");
+	}
 	public static void main(String[]args) {
+		Sample s=new Sample();
+		s.show();
 System.out.println("Hello");
 }
 }
